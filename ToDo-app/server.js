1e802f99ef545badbd18a76c925775fb5d46e888
MxAziz <hereabdulaziz@gmail.com>
2025-06-15 01:36:34 +0600

    Creating and Reading A ToDo using Postman

--- ToDo-app/server.js
+++ ToDo-app/server.js
@@ -8,18 +8,44 @@ const server = http.createServer((req, res) => {
     const method = req.method;
     const url = req.url;
 
+    //GET all todos
     if (method === "GET", url === "/todos") {
         const data = fs.readFileSync(filePath, 'utf-8');
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(data);
-    } else if (method === "POST", url === "/todoss") {
-        res.end("Todo created successfully");
+    }
+    //post a new todo
+    else if (method === "POST", url === "/todos/create-todo") {
+        let data = '';
+        req.on('data', (chunk) => {
+            data = data + chunk;
+        })
+
+        req.on('end', () => {
+            const {title, body} = JSON.parse(data);
+
+            const createdAt = new Date().toLocaleString();
+
+        const AllTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+            console.log(AllTodos);
+
+            // Parse the existing todos
+            const todos = JSON.parse(AllTodos || '[]');
+
+            todos.push({ title, body, createdAt });
+
+            fs.writeFileSync(filePath, JSON.stringify(todos, null, 2), {encoding: "utf-8"});
+
+            res.end(JSON.stringify({ title, body, createdAt },null, 2));
+
+         })
+
     } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Route not found');
   }
 
-    res.end('welcome to the ToDo app server');
+    // res.end('welcome to the ToDo app server');
 });
 
 
